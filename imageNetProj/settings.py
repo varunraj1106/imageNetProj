b195e55c9271bd54537ffe8c612f263c7ba7b965
varunraj1106 <86312464+varunraj1106@users.noreply.github.com>
2022-02-01 22:34:43 +0530

    Update settings.py

--- imageNetProj/settings.py
+++ imageNetProj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^7+ldu!c-2f68#(fw-j+3j@#t+wz%*77-xgl@lb4q75x6i5o)8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','imagenetproj.herokuapp.com']
 
