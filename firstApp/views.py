7a4054d1df41e986dc8924ec30fd62c877ee13c8
varunraj1106 <86312464+varunraj1106@users.noreply.github.com>
2022-03-23 16:03:13 +0530

    Update views.py

--- firstApp/views.py
+++ firstApp/views.py
@@ -72,10 +72,10 @@ def predictImage(request):
     #pytesseract.pytesseract.tesseract_cmd = ‘/app/.apt/usr/bin/tesseract’
     pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
     text = 'abc'
-    text = str(text)
+    #text = str(text)
     text = pytesseract.image_to_string(img)
     t2 = text 
-    
+    t2 = str(t2)
     
     
     
